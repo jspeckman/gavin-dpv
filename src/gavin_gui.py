786bb3ed3121f999b9062e99e10ad0e88e6e6f24
Jason Speckman <jspeckman@gmail.com>
2018-05-07 20:45:04 -0500

    Initial work on web GUI to get battery percent and start/stop logging from mobile browser

--- src/gavin_gui.py
+++ src/gavin_gui.py
@@ -1,64 +1,39 @@
-#!/usr/bin/python
+#!/usr/bin/python3.6 -u
 
-import os
-import json
-import threading
-import time
-import datetime
-from flask import *
+from http.server import BaseHTTPRequestHandler, HTTPServer
 
-# setup config maps
-config_map = {}
-battery_map = {}
-imu_axis_map = {}
+id = 'Gavin GUI'
+version = '0.0.1'
+port = 8080
 
-# Config file location
-config_map['config_file'] = "./config.json"
-# Logging directory
-config_map['log_dir'] = "."
-# Battery logfile
-config_map['batt_file_prefix'] = "battery.log-"
+class gavin_gui(BaseHTTPRequestHandler):
+    def _set_headers(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
 
-config_map['FORCE_CALIBRATION'] = 0
-config_map['log_file'] = ""
-config_map['begin_logging'] = 0
-config_map['epoch_counter'] = int(time.time())
-config_map['eod_delay'] = 300
-battery_map['initial_ert'] = 65535
-battery_map['epoch_counter'] = 0
+    def do_GET(self):
+        self._set_headers()
+        self.wfile.write(bytes("<html><body><h1>Battery</h1>", "utf-8"))
+        self.wfile.write(bytes("<h1>Percent Here</h1></body></html>", "utf-8"))
 
-# Default config values
-config_map['env_port'] = 3030
-config_map['bms_port'] = 2135
-config_map['imu_port'] = 1845
-config_map['log_port'] = 1270
+    def do_HEAD(self):
+        self._set_headers()
+        
+    def do_POST(self):
+        # Doesn't do anything with posted data
+        content_length = int(self.headers['Content-Length']) # <--- Gets the size of data
+        post_data = self.rfile.read(content_length) # <--- Gets the data itself
+        self._set_headers()
+        self.wfile.write(bytes("<html><body><h1>POST!</h1></body></html>", "utf-8"))
+        print(post_data)
+        
+def run(server_class=HTTPServer, handler_class=gavin_gui, port=80):
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    print('Starting httpd...')
+    httpd.serve_forever()
 
-config_map['debug'] = 0
-config_map['first_run'] = "YES"
-config_map['uuid'] = ""
-config_map['myname'] = "DPV"
-config_map['bno055calibration'] = ""
-config_map['motor_amps'] = 16 # Gavin motor
-config_map['units'] = "Imperial"
-config_map['bno_update_hz'] = 10
-config_map['sample_rate'] = 1
-config_map['activate_method'] = "Pressure"
-config_map['activate_trigger'] = 900
+print(id,  version,  "listening on port 80")
 
-# Default battery values
-battery_map['logfile'] = logfiles.get_battery_file(config_map['log_dir'], config_map['batt_file_prefix'])
-battery_map['mfg'] = ""
-battery_map['model'] = ""
-battery_map['weight'] = 0
-battery_map['modules'] = 2
-battery_map['chemistry'] = "SLA"
-battery_map['voltage'] = 12
-battery_map['amphr'] = 35
-
-# Default BNO055 mapping values
-BNO_AXIS_MAP['x'] = AXIS_REMAP_X
-BNO_AXIS_MAP['y'] = AXIS_REMAP_Y
-BNO_AXIS_MAP['z'] = AXIS_REMAP_Z
-BNO_AXIS_MAP['x_sign'] = AXIS_REMAP_POSITIVE
-BNO_AXIS_MAP['y_sign'] = AXIS_REMAP_POSITIVE
-BNO_AXIS_MAP['z_sign'] = AXIS_REMAP_POSITIVE
+run(port=port)

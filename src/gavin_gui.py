13cdc89e68d01e09acf4be557c7727f6ced6a9dd
Jason Speckman <jason.t.speckman-1@ou.edu>
2018-05-08 15:09:52 -0500

    Displays battery percent, ERT, and start/stop logging via button

--- src/gavin_gui.py
+++ src/gavin_gui.py
@@ -6,7 +6,9 @@ import socket
 
 id = 'Gavin GUI'
 version = '0.0.1'
+data_hub_socket = '/tmp/gavin_data_hub.socket'
 port = 8080
+
 
 class gavin_gui(BaseHTTPRequestHandler):
     def _set_headers(self):
@@ -16,10 +18,31 @@ class gavin_gui(BaseHTTPRequestHandler):
 
     def do_GET(self):
         battery_percent,  battery_ert = self.get_battery_percent()
+        logging_status = self.logging_status()
+        
         self._set_headers()
-        self.wfile.write(bytes("<html><body><h1><center>Battery</center></h1>", "utf-8"))
-        self.wfile.write(bytes("<h1><center>%s</center></h1></body></html>" % (battery_percent), "utf-8"))
-
+        self.wfile.write(bytes("<html><body>", "utf-8"))
+        self.wfile.write(bytes('<center><h1>Battery</h1></center>', "utf-8"))
+        if battery_percent >= 75:
+            self.wfile.write(bytes('<center><p style="font-size:30px;color:green"><strong>%d %%</strong></p></center>' % (battery_percent), "utf-8"))
+        elif 63 <= battery_percent < 75:
+            self.wfile.write(bytes('<center><p style="font-size:30px;color:orange"><strong>%d %%</strong></p></center>' % (battery_percent), "utf-8"))
+        elif 0 <= battery_percent < 63:
+            self.wfile.write(bytes('<center><p style="font-size:30px;color:red"><strong>%d %%</strong></p></center>' % (battery_percent), "utf-8"))
+        elif battery_percent == -1:
+            self.wfile.write(bytes('<center><p style="font-size:30px;color:red"><strong>Unable to get battery status</strong></p></center>', "utf-8"))
+        self.wfile.write(bytes('<center><p style="font-size:20px;">Estimated Runtime:</p></center>', "utf-8"))
+        self.wfile.write(bytes('<center><p style="font-size:20px;">%d Minutes</p></center>' % (battery_ert), "utf-8"))
+        self.wfile.write(bytes('<p></p>',  "utf-8"))
+        if logging_status == 0:
+            self.wfile.write(bytes('<center><p style="font-size:20px;"><form action="/" method="post"><input type="hidden" name="logging" value="start"><button type="submit">Start Logging</button></form></p></center>', "utf-8"))
+        elif logging_status == 1:
+            self.wfile.write(bytes('<center><p style="font-size:20px;"><form action="/" method="post"><input type="hidden" name="logging" value="stop"><button type="submit">Stop Logging</button></p></center>', "utf-8"))
+        elif logging_status == -1:
+             self.wfile.write(bytes('<center><p style="font-size:30px;color:red"><strong>Unable to get logging status</strong></p></center>', "utf-8"))
+        
+        self.wfile.write(bytes("</body></html>", "utf-8"))
+        
     def do_HEAD(self):
         self._set_headers()
         
@@ -27,19 +50,25 @@ class gavin_gui(BaseHTTPRequestHandler):
         # Doesn't do anything with posted data
         content_length = int(self.headers['Content-Length']) # <--- Gets the size of data
         post_data = self.rfile.read(content_length) # <--- Gets the data itself
-        self._set_headers()
-        self.wfile.write(bytes("<html><body><h1>POST!</h1></body></html>", "utf-8"))
-        print(post_data)
+        if 'logging=start' in str(post_data):
+            self.start_stop_logging(1)
+        elif 'logging=stop' in str(post_data):
+            self.start_stop_logging(0)
+        self.do_GET()
+
+    def log_message(self, format, *args):
+        return
         
     def get_battery_percent(self):
-        data_hub_socket = '/tmp/gavin_data_hub.socket'
         connect_failed = 0
         sensorsocket = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
         sensor_address = data_hub_socket
         try:
             sensorsocket.connect(sensor_address)
         except:
-            print("unable to connect to Gavin data hub daemon")
+            print("unable to connect to Gavin Data Hub daemon")
+            percent = -1
+            ert = -1
             connect_failed = 1
         if connect_failed == 0:
             try:
@@ -51,20 +80,84 @@ class gavin_gui(BaseHTTPRequestHandler):
                     sensorsocket.close()
                     return
                 if len(data) > 0:
-                    percent = data['percent']
-                    ert = data['ert'] / 60
+                    if data['percent']:
+                        percent = int(data['percent'])
+                    else:
+                        percent = -1
+                    if data['ert']:
+                        ert = int(data['ert'])
+                    else:
+                        ert = -1
             except socket.error:
                 print("unable to request from",  id)
+                percent = -1
+                ert = -1
+        else:
+            percent = -1
+            ert = -1
         
         sensorsocket.close()
         return(percent,  ert)
 
+    def start_stop_logging(self,  logging_enable):
+        connect_failed = 0
+        sensorsocket = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+        sensor_address = data_hub_socket
+        try:
+            sensorsocket.connect(sensor_address)
+        except:
+            print("unable to connect to Gavin Data Hub daemon")
+            connect_failed = 1
+        if connect_failed == 0:
+            try:
+                if logging_enable == 1:
+                    msg = '{"request":"logging start"}'
+                else:
+                    msg = '{"request":"logging stop"}'
+                sensorsocket.send(msg.encode())
+                #data = sensorsocket.recv(512).decode()
+            except socket.error:
+                print("unable to request logging activation")
+        
+        sensorsocket.close()
+        
+    def logging_status(self):
+        connect_failed = 0
+        sensorsocket = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+        sensor_address = data_hub_socket
+        try:
+            sensorsocket.connect(sensor_address)
+        except:
+            print("unable to connect to Gavin Data Hub daemon")
+            logging_enabled = -1
+            connect_failed == 1
+        
+        if connect_failed == 0:
+            try:
+                msg = '{"request":"logging status"}'
+                sensorsocket.send(msg.encode())
+                data = sensorsocket.recv(512).decode()
+                if 'running' in data:
+                    logging_enabled = 1
+                elif 'stopped' in data:
+                    logging_enabled = 0
+            except socket.error:
+                print("unable to request logging status")
+                logging_enabled = -1
+                
+        else:
+            logging_enabled = -1
+            
+        sensorsocket.close()
+        
+        return(logging_enabled)
+        
 def run(server_class=HTTPServer, handler_class=gavin_gui, port=80):
     server_address = ('', port)
     httpd = server_class(server_address, handler_class)
     print('Starting httpd...')
     httpd.serve_forever()
 
-print(id,  version,  "listening on port 80")
+print(id,  version,  "listening on port %d" % (port))
 
 run(port=port)
